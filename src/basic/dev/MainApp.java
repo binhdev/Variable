8187b3a10d90dabd3074d71435fa69b00b4858f8
Thanh Binh <ttbinh@hueic.edu.vn>
2022-01-26 17:50:17 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,17 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstName = "Phan Van";
+		String lastName = "Nam";
+		int old = 15;
+		float markMath = 9f;
+		boolean passed = true;
+		System.out.println("Ho va ten");
+		System.out.println(firstName + " " + lastName);
+		
+	}
+
+}
